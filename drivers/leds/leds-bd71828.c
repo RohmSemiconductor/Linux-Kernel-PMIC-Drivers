607d9caa9256f6035ab156ce55a0769159017b99
Matti Vaittinen <matti.vaittinen@fi.rohmeurope.com>
2019-05-20 10:04:03 +0300

    led: bd71828: Support LED outputs on ROHM BD71828 PMIC
    ROHM BD71828 power management IC has two LED outputs for charge status
    and button pressing indications. The LED outputs can also be forced
    bs SW so add driver allowing to use these LEDs for other indications
    as well.
    
    Leds are controlled by SW using 'Force ON' bits. Please note the
    constrains mentioned in data-sheet:
    1. If one LED is forced ON - then also the other LED is forced.
    	=> You can't use SW control to force ON one LED and allow HW
    	   to control the other.
    2. You can't force both LEDs OFF. If the FORCE bit for both LED's is
       zero, then LEDs are controlled by HW and indicate button/charger
       states as explained in data-sheet.
    
    Signed-off-by: Matti Vaittinen <mazziesaccount@gmail.com>
    
    ---
    100% untested

---
+++ drivers/leds/leds-bd71828.c
@@ -0,0 +1,105 @@
+// SPDX-License-Identifier: GPL-2.0
+// Copyright (C) 2019 ROHM Semiconductors
+
+#include <linux/device.h>
+#include <linux/err.h>
+#include <linux/kernel.h>
+#include <linux/leds.h>
+#include <linux/mfd/rohm-generic.h>
+#include <linux/mfd/rohm-bd71828.h>
+#include <linux/module.h>
+#include <linux/mod_devicetable.h>
+#include <linux/platform_device.h>
+#include <linux/regmap.h>
+#include <linux/slab.h>
+
+#define BD71828_LED_TO_DATA(l) ((l)->id == ID_GREEN_LED ? \
+	container_of((l), struct bd71828_leds, green) : \
+	container_of((l), struct bd71828_leds, amber))
+
+enum {
+	ID_GREEN_LED,
+	ID_AMBER_LED,
+	ID_NMBR_OF,
+};
+
+struct bd71828_led {
+	int id;
+	struct led_classdev l;
+	u8 force_mask;
+};
+
+struct bd71828_leds {
+	struct regmap *regmap;
+	struct bd71828_led green;
+	struct bd71828_led amber;
+	u8 force_reg;
+};
+
+static int bd71828_led_brightness_set(struct led_classdev *led_cdev,
+                                        enum led_brightness value)
+{
+	struct bd71828_led *l = container_of(led_cdev, struct bd71828_led, l);
+	struct bd71828_leds *data;
+	unsigned val = BD71828_LED_OFF;
+
+	data = BD71828_LED_TO_DATA(l);	
+	if (value != LED_OFF)
+		val = BD71828_LED_ON;
+
+	return regmap_update_bits(data->regmap, BD71828_REG_LED_CTRL,
+			    l->force_mask, val);
+}
+
+static int bd71828_led_probe(struct platform_device *pdev)
+{
+	struct bd71828_leds *l;
+	struct bd71828_led *g, *a;
+	struct regmap *r;
+	int ret;
+
+	r = dev_get_regmap(pdev->dev.parent, NULL);
+	if (!r)
+		return dev_err_probe(&pdev->dev, -ENODEV, "No regmap");
+
+	l = devm_kzalloc(&pdev->dev, sizeof(*l), GFP_KERNEL);
+	if (l)
+		return -ENOMEM;
+
+	l->regmap = r;
+	a = &l->amber;
+	g = &l->green;
+	a->id = ID_AMBER_LED;
+	g->id = ID_GREEN_LED;
+	a->force_mask = BD71828_MASK_LED_AMBER;
+	g->force_mask = BD71828_MASK_LED_GREEN;
+
+	a->l.brightness_set_blocking = bd71828_led_brightness_set;
+	g->l.brightness_set_blocking = bd71828_led_brightness_set;
+
+	ret = devm_led_classdev_register(&pdev->dev, &g->l);
+	if (ret)
+		return ret;
+
+	return devm_led_classdev_register(&pdev->dev, &a->l);
+}
+
+static const struct platform_device_id bd71828_led_id[] = {
+	{ "bd71828-led", ROHM_CHIP_TYPE_BD71828 },
+	{ },
+};
+MODULE_DEVICE_TABLE(platform, bd71828_led_id);
+
+static struct platform_driver bd71828_led_driver = {
+        .driver = {
+                .name  = "bd71828-led",
+        },
+        .probe  = bd71828_led_probe,
+	.id_table = bd71828_led_id,
+};
+
+module_platform_driver(bd71828_led_driver);
+
+MODULE_AUTHOR("Matti Vaittinen <matti.vaittinen@fi.rohmeurope.com>");
+MODULE_DESCRIPTION("ROHM BD71828 LED driver");
+MODULE_LICENSE("GPL");
