475de346c55b3ab822e97ebc9f67d8a203018008
Matti Vaittinen <matti.vaittinen@fi.rohmeurope.com>
2021-09-27 15:39:43 +0300

    jenkins-tests: make log creating script less verbose
    The debug information from the log creation script is making spotting
    the real failure harder because the logs are collected _after_ the error
    has been detected - so the prints are issued after the actual fault
    prints. The log creation prints have little value after log cretion
    is proven working.
    
    Remove the prints to make actual errors in logs more visible.
    
    Signed-off-by: Matti Vaittinen <matti.vaittinen@fi.rohmeurope.com>

--- jenkins-tests/scripts/tar_and_clean_logs.sh
+++ jenkins-tests/scripts/tar_and_clean_logs.sh
@@ -4,15 +4,15 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
 
 #JENKINS_LOG_STORAGE=/var/jenkins/workspace/PMIC_testing_job/logs
 
-echo "moving old tar(s): mv $RUNCFG_LOGDIR/testlogs//results_*.tar.gz $RUNCFG_LOGDIR/oldlogs/."
+#echo "moving old tar(s): mv $RUNCFG_LOGDIR/testlogs//results_*.tar.gz $RUNCFG_LOGDIR/oldlogs/."
 mv $RUNCFG_LOGDIR/testlogs//results_*.tar.gz $RUNCFG_LOGDIR/oldlogs/.
 
 D2=`date +%Y%m%d_%s`
 
-echo "Creating new tar: tar -cvzf $RUNCFG_LOGDIR/testlogs/results_$D2.tar.gz $LOGS"
+#echo "Creating new tar: tar -cvzf $RUNCFG_LOGDIR/testlogs/results_$D2.tar.gz $LOGS"
 tar -cvzf $RUNCFG_LOGDIR/testlogs/results_$D2.tar.gz $LOGS
 #|| exit -1
 
-echo "cleaning logs rm -rf $LOGS"
+#echo "cleaning logs rm -rf $LOGS"
 rm -rf $LOGS
 
