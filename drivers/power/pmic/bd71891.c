aae5f1e399e8402da81283abd520f1aa56e538b8
Matti Vaittinen <mazziesaccount@gmail.com>
2025-03-28 12:41:20 +0200

    pmic: bd71891: Add chipinfo command
    Add a debugging command for printing PMIC state information. The command
    prints:
     - Stored PMIC boot diagnostics
     - Current internal state machine state.
    
    This should be helpful both for debugging, and also for serving as a
    code example about reading the reset / boot state registers.
    
    Signed-off-by: Matti Vaittinen <mazziesaccount@gmail.com>
    ---
    100% Untested. Also, it may be the RESETSRC2_FLG and RESETSRC3_FLG need
    only to be read if BIT(6) is set in the RESETSRC1_FLG register. This,
    however, is a minimal optimization.

--- drivers/power/pmic/bd71891.c
+++ drivers/power/pmic/bd71891.c
@@ -4,6 +4,7 @@
  *  Matti Vaittinen <mazziesaccount@gmail.com>
  */
 
+#include <command.h>
 #include <common.h>
 #include <div64.h>
 #include <dm.h>
@@ -16,6 +17,21 @@
 #include <power/regulator.h>
 #include <power/bd71891.h>
 #include "bdxxxx.h"
+
+#define PMIC_DT_NAME "pmic@4b"
+
+
+DECLARE_GLOBAL_DATA_PTR;
+
+static inline int bd71891_pr_reas_bf(const struct reason_reg *r)
+{
+	return print_reason_bf_reg(PMIC_DT_NAME, r);
+}
+
+static inline int bd71891_pr_reas_field(const struct reason_reg_field *f)
+{
+	return print_reason_field_reg(PMIC_DT_NAME, f);
+}
 
 static const struct pmic_child_info pmic_children_info[] = {
 	/* boost */
@@ -125,4 +141,147 @@ U_BOOT_DRIVER(pmic_bd71891) = {
 	.ops = &bd71891_ops,
 };
 
+/* Commands */
+
+const struct reason_info bd71891_boot_reas_info[] = {
+	REASON_INFO("Cold reset", BIT(7)),
+	REASON_INFO("Emergency recovery", BIT(6)),
+	REASON_INFO("GATE_DRV open", BIT(2)),
+	REASON_INFO("PMIC cold boot", BIT(1)),
+	REASON_INFO("USB characterization completed", BIT(0)),
+};
+
+const struct reason_info bd71891_reset_reas_info_0[] = {
+	REASON_INFO("External dev asserted FLT_B", BIT(7)),
+	REASON_INFO("Voltage fault", BIT(6)),
+	REASON_INFO("Bad VSYS Voltage", BIT(4)),
+	REASON_INFO("Thermal Shuitdown", BIT(3)),
+	REASON_INFO("PMIC_EN pin toggles", BIT(2)),
+	REASON_INFO("SWRESET", BIT(1)),
+	REASON_INFO("Watchdog", BIT(0)),
+};
+
+const struct reason_info bd71891_reset_reas_info_1[] = {
+	REASON_INFO("LDO2 Power Failure", BIT(6)),
+	REASON_INFO("LDO1 Power Failure", BIT(5)),
+	REASON_INFO("BOOST Power Failure", BIT(4)),
+	REASON_INFO("BUCK4 Power Failure", BIT(3)),
+	REASON_INFO("BUCK3 Power Failure", BIT(2)),
+	REASON_INFO("BUCK2 Power Failure", BIT(1)),
+	REASON_INFO("BUCK1 Power Failure", BIT(0)),
+};
+
+const struct reason_info bd71891_reset_reas_info_2[] = {
+	REASON_INFO("HDMI internal V3P3 VRFLT_RST", BIT(5)),
+	REASON_INFO("BOOST HOCP", BIT(4)),
+	REASON_INFO("BUCK4 OCP", BIT(3)),
+	REASON_INFO("BUCK3 OCP", BIT(2)),
+	REASON_INFO("BUCK2 OCP", BIT(1)),
+	REASON_INFO("BUCK1 OCP", BIT(0)),
+};
+
+const struct reason_info bd71891_power_state_info[] = {
+	REASON_INFO("IC Shutdown", 0),
+	REASON_INFO("Emergency", 1),
+	REASON_INFO("DEAD", 2),
+	REASON_INFO("OTP LOAD", 3),
+	REASON_INFO("HIBERNATE", 4),
+	REASON_INFO("RUN", 5),
+	REASON_INFO("IDLE", 6),
+	REASON_INFO("USB characterization", 7),
+};
+
+const struct reason_reg bd71891_boot_reas = {
+	.explanation = "BOOT FLAGS",
+	.reasons = &bd71891_boot_reas_info[0],
+	.num_reasons = ARRAY_SIZE(bd71891_boot_reas_info),
+	.reg = BD71891_REG_BOOTSRC,
+};
+
+const struct reason_reg bd71891_reset_reas[] =
+{
+	{
+		.explanation = "Reset reason flag 1 (RESETSRC1_FLG 0x08)",
+		.reasons = &bd71891_reset_reas_info_0[0],
+		.num_reasons = ARRAY_SIZE(bd71891_reset_reas_info_0),
+		.reg = BD71891_REG_RESETSRC1,
+	}, {
+		.explanation = "Reset reason flag 2 (RESETSRC2_FLG 0x09)",
+		.reasons = &bd71891_reset_reas_info_1[0],
+		.num_reasons = ARRAY_SIZE(bd71891_reset_reas_info_1),
+		.reg = BD71891_REG_RESETSRC2,
+	}, {
+		.explanation = "Reset reason flag 3 (RESETSRC3_FLG 0x0a)",
+		.reasons = &bd71891_reset_reas_info_2[0],
+		.num_reasons = ARRAY_SIZE(bd71891_reset_reas_info_2),
+		.reg = BD71891_REG_RESETSRC3,
+	},
+};
+
+const struct reason_reg_field bd71891_power_state = {
+	.reason_reg = {
+		.explanation = "PMIC state",
+		.reasons = &bd71891_power_state_info[0],
+		.num_reasons = ARRAY_SIZE(bd71891_power_state_info),
+		.reg = BD71891_REG_POWER_STATE,
+	},
+	.mask = 0x3,
+};
+
+/* Get the power-on reason */
+static int do_chipinfo(struct cmd_tbl *cmdtp, int flag, int argc,
+		   char *const argv[])
+{
+	int ret, i;
+
+	ret = bd71891_pr_reas_bf(&bd71891_boot_reas);
+	if (ret)
+		return cmd_failure(ret);
+
+	for (i = 0; i < ARRAY_SIZE(bd71891_reset_reas); i++) {
+		ret = bd71891_pr_reas_bf(&bd71891_reset_reas[i]);
+		if (ret)
+			return cmd_failure(ret);
+	}
+
+	ret = bd71891_pr_reas_field(&bd71891_power_state);
+	return cmd_ret(ret);
+}
+
+static struct cmd_tbl subcmd[] = {
+	U_BOOT_CMD_MKENT(chipinfo, 1, 1, do_chipinfo, "", ""),
+	/*U_BOOT_CMD_MKENT(dt_init, 1, 1, do_dt_init, "", ""),
+	U_BOOT_CMD_MKENT(hibernate, 1, 1, do_hibernate, "", ""),
+	U_BOOT_CMD_MKENT(get_state, 1, 1, do_get_state, "", ""),
+	U_BOOT_CMD_MKENT(set_state, 2, 1, do_set_state, "", ""),
+	U_BOOT_CMD_MKENT(adc_state, 2, 1, do_adc_state, "", ""),
+	U_BOOT_CMD_MKENT(adc_source, 2, 1, do_adc_source, "", ""),
+	U_BOOT_CMD_MKENT(adc_vol_source, 2, 1, do_adc_vol_source, "", ""),
+	U_BOOT_CMD_MKENT(adc_gain, 2, 1, do_adc_gain, "", ""),
+	U_BOOT_CMD_MKENT(adc_meas, 4, 1, do_adc_meas, "", ""),
+	U_BOOT_CMD_MKENT(adc_limit, 3, 1, do_adc_limit, "", ""),
+	U_BOOT_CMD_MKENT(adc_get, 2, 1, do_adc_get, "", ""),*/
+};
+
+static int do_bd71891(struct cmd_tbl *cmdtp, int flag, int argc,
+		   char *const argv[])
+{
+	struct cmd_tbl *cmd;
+
+	argc--;
+	argv++;
+
+	cmd = find_cmd_tbl(argv[0], subcmd, ARRAY_SIZE(subcmd));
+	if (cmd == NULL || argc > cmd->maxargs)
+		return CMD_RET_USAGE;
+
+	return cmd->cmd(cmdtp, flag, argc, argv);
+}
+
+
+
+U_BOOT_CMD(bd71891, CONFIG_SYS_MAXARGS, 1, do_bd71891,
+	"BD71891 sub-system",
+	"bd71891 chipinfo - recorded power-on reasons and current power state\n"
+);
 
